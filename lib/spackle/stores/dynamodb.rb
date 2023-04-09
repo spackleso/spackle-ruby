cf2065b74400a06ec37d73e1a49e126597df664c
Hunter Clarke <hunter@hunterclarke.me>
2023-04-09 14:36:35 -0600

    moves session creation to credentials

--- lib/spackle/stores/dynamodb.rb
+++ lib/spackle/stores/dynamodb.rb
@@ -6,11 +6,10 @@ require 'logger'
 module Spackle
   class DynamoDBStore < BaseStore
     @client = nil
-    @store_config = {}
+    @credentials = nil
 
-    def initialize(client = nil, store_config = nil)
-      @store_config = store_config || {}
-      @client = client || bootstrap_client
+    def initialize()
+      @client = bootstrap_client
     end
 
     def get_customer_data(id)
@@ -27,9 +26,9 @@ module Spackle
 
     def get_item(id)
       return @client.get_item({
-        table_name: @store_config['table_name'],
+        table_name: @credentials.session['adapter']['table_name'],
         key: {
-          AccountId: @store_config['identity_id'],
+          AccountId: @credentials.session['adapter']['identity_id'],
           CustomerId: "#{id}:#{Spackle.version}"
         }
       })
@@ -44,6 +43,37 @@ module Spackle
         raise SpackleError.new 'API key not set'
       end
 
+      @credentials = SpackleAWSCredentials.new
+      Aws::DynamoDB::Client.new(
+        region: @credentials.session['adapter']['region'],
+        credentials: @credentials,
+      )
+    end
+  end
+
+  class SpackleAWSCredentials
+    include Aws::CredentialProvider
+    include Aws::RefreshingCredentials
+
+    @client = nil
+    @session = {}
+
+    def initialize()
+      @session = create_session
+      @client = Aws::STS::Client.new(
+        region: @session['adapter']['region'],
+        credentials: false,
+      )
+      super()
+    end
+
+    def session
+      @session
+    end
+
+    private
+
+    def create_session
       uri = URI(Spackle.api_base + '/sessions')
       https = Net::HTTP.new(uri.host, uri.port)
       https.use_ssl = true
@@ -54,48 +84,15 @@ module Spackle
       response = https.request(request)
       data = JSON.parse(response.body)
       Util.log_debug("Created session: #{data}")
-
-      @store_config = data['adapter']
-      credentials = SpackleCredentials.new(
-        @store_config['role_arn'],
-        @store_config['token'],
-        @store_config['region'],
-      )
-
-      Aws::DynamoDB::Client.new(
-        region: @store_config['region'],
-        credentials: credentials,
-      )
+      data
     end
-  end
-
-  class SpackleCredentials
-    include Aws::CredentialProvider
-    include Aws::RefreshingCredentials
-
-    @region = nil
-    @role_arn = nil
-    @token = nil
-
-    def initialize(role_arn, token, region)
-      @region = region
-      @role_arn = role_arn
-      @token = token
-      @client = Aws::STS::Client.new(
-        region: @region,
-        credentials: false,
-      )
-      super()
-    end
-
-    private
 
     def refresh
       Util.log_debug('Refreshing DynamoDB credentials...')
       c = @client.assume_role_with_web_identity({
-        role_arn: @role_arn,
+        role_arn: @session['adapter']['role_arn'],
         role_session_name: Base64.strict_encode64(SecureRandom.uuid),
-        web_identity_token: @token
+        web_identity_token: @session['adapter']['token'],
       }).credentials
       @credentials = Aws::Credentials.new(
         c.access_key_id,
